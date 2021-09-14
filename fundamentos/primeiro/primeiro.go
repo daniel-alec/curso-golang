48a370a7bfef8f32c559f8a282214fd786691b2d
Daniel Alexandre Cordeiro <daniel.cordeiro@simepar.br>
2021-09-14 02:13:53 -0300

    Init - first commit
    Signed-off-by: Daniel Alexandre Cordeiro <daniel.cordeiro@simepar.br>

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+// Programa executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-lo é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de um programa Go é uma função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+	/*
+		Sobre comentários...
+
+		1) Priorize código legível e faça comentários que agrega valor!
+		2) Evite comentários óbvios
+		3) Durante o curso abuse dos comentários
+	*/
+}
